aaeef90512291989e887d1a83b57fb07c14fda57
bekky <bekky@LAPTOP-4HT0DL1A>
2022-05-07 00:19:37 +1000

    Add project files.

---
+++ LearningZone1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LearningZone1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
